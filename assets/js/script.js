59436b5673abe37bf70b2fc1f1f7e312ca8aac98
FelipeSDS23 <felipe03san@gmail.com>
2024-06-11 20:29:05 -0300

    Menu ao redimencionar a janela corrigido

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,15 +2,25 @@
 const open_close_btn = document.querySelector("#open-close-btn");
 const header = document.querySelector("#header");
 
-open_close_btn.addEventListener("click", () => {
+window.addEventListener("resize", resize);
+open_close_btn.addEventListener("click", showHideMobileMenu);
 
+function showHideMobileMenu() {
     if(header.style.display == "flex") { // Se o menu mobile estiver aparecendo
         header.style.display = "none"; 
         open_close_btn.innerText = "menu";
     } else {
         header.style.display = "flex";
         open_close_btn.innerText = "close";
+    } 
+}
+
+function resize() { // Mostra/esconde o menu ao redimencionar a janela
+    open_close_btn.innerText = "menu";
+    if (innerWidth >= 800) {
+        header.removeAttribute("style");
+    } else {
+        header.style.display = "none";
     }
-    
-});
+}
 // Fim controle do menu (mobile / desktop)
